cf6d4bea46567f73f67a3b46e19ed9680b8b4be9
Mathéo <matheo.galu56@gmail.com>
2022-03-10 16:40:18 -0500

    oups

--- server/list.php
+++ server/list.php
@@ -4,8 +4,8 @@ header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With");
 header('Content-Type: application/json; charset=utf-8');
 
-require_once("projectDAO.php");
+require_once("ProjectDAO.php");
 
-$listProject = projectDAO::lister();
+$listProject = ProjectDAO::lister();
 echo json_encode($listProject);
 
